10832e4c8142ab95daefbd00675be4c365ab8f72
alexlee <2661646649@qq.com>
2023-05-23 23:49:13 +0800

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // content: ["./src/**/*.{html,vue,js}"],
+  // content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
